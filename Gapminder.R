dbe03873ad27de1503326bddd0e13b091c6e6dbf
Jaime Andres Salas <jsalasmoreira@gmail.com>
2020-04-27 15:43:06 -0500

    Creacion de primer modelo Lineal

--- Gapminder.R
+++ Gapminder.R
@@ -31,3 +31,44 @@ ec%>%
   geom_line()+
   ggtitle("Residual Pattern")+
   theme(plot.title = element_text(hjust = 0.5))
+
+
+
+#DAtaframe anidado
+
+by_country <- gapminder%>%
+  group_by(country, continent)%>%
+  nest()
+by_country%>%View()
+
+by_country $data[[1]]
+
+country_model <- function(df){
+  lm(lifeExp~year, data=df)
+}
+
+models <- map(by_country$data, country_model)
+
+by_country <- by_country%>%
+  mutate(model=map(data,country_model))
+
+by_country%>%
+  filter(continent=="Europe")
+
+by_country <- by_country%>%
+  mutate(resids= map2(data, model,add_residuals))
+
+by_country
+
+resids <- unnest(by_country, resids)
+resids
+
+resids%>%
+  ggplot(aes(year,resid))+
+  geom_line(aes(group=country),alpha=0.2)+
+  geom_smooth(se=FALSE)
+resids%>%
+  ggplot(aes(year,resid))+
+  geom_line(aes(group=country),alpha=0.2)+
+  geom_smooth(se=FALSE)+
+  facet_wrap(~continent)
